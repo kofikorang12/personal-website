a1074b7e899a56b5141363c97990a1b607050bf5
Jeffrey Baah <jbaah69@gmail.com>
2023-04-21 12:50:37 -0700

    sendfile index

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", function(request, response){
-	
+app.get("/", function(req, res){
+	res.sendFile(__dirname + "/index.html");
 })
 
-app.listen(3000, function);
+app.listen(3000, function(){
+	console.log("Server started on port 3000");
+});
